0ed77221ea421634b6bf66816cf174d523e9250d
Alan Kusparmakov <alan.kusparmakov@gmail.com>
2026-09-04 14:46:59 -0700

    Working counter

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -5,8 +5,19 @@
 
 #include "stm32l4xx_it.h"
 #include <stdbool.h>
+#include <stdint.h>
 
-#define GPIO_MODER_MODE_OUTPUT 0b01
+/* Constant parameters */
+
+/* What if we want our display delay to be exact, from the user's perspective? Here we basically
+ * Account for the other delay that we do by direct subtraction, but that basically assumes that 
+ * button_delay performs a delay exactly once. If we change some logic inside of button_delay,
+ * would it be possible to automatically account its total delay consumption (preferrably at compile time), 
+ * and then factor that into DISPLAY_DELAY?
+ */
+
+#define BUTTON_DELAY 25
+#define DISPLAY_DELAY (750 - BUTTON_DELAY)
 
 void gpio_init()
 {
@@ -34,23 +45,55 @@ void gpio_init()
 	GPIOA->PUPDR |= (GPIO_PULLDOWN << GPIO_PUPDR_PUPD4_Pos);
 }
 
-bool button_pressed()
+bool PCA_is_high()
 {
 	return GPIOA->IDR & GPIO_IDR_ID4;
 }
 
-void LED_on()
+void button_delay()
+{
+	HAL_Delay(BUTTON_DELAY);
+}
+
+// check for button state while accounting for button bounce
+
+/* We tried to hide implementation details inside function bodies, in order for the main function
+ * to be entirely logic based. At the same time, we wanted our functions to be "pure" or "honest"
+ * in the sense that they don't rely on or modify some kind of global state. This also meant that 
+ * we tried to isolate and well-define the functions that interact with some kind of external world
+ * such as button check and display. 
+ * */
+bool button_is_pressed()
+{
+	if (PCA_is_high()) {
+		button_delay();
+
+		if (PCA_is_high())
+			return true;
+	}
+
+	return false;
+}
+
+void LED_turn_on()
 {
 	GPIOC->ODR |= GPIO_ODR_OD0;
 	GPIOC->ODR |= GPIO_ODR_OD1;
 	GPIOC->ODR |= GPIO_ODR_OD2;
 }
 
-void LED_off()
+void LED_turn_off()
 {
 	GPIOC->ODR &= ~GPIO_ODR_OD0;
 	GPIOC->ODR &= ~GPIO_ODR_OD1;
 	GPIOC->ODR &= ~GPIO_ODR_OD2;
+}
+
+void count_display(uint8_t count)
+{
+	LED_turn_off();
+	GPIOC->ODR |= ((count) << GPIO_ODR_OD0_Pos);
+	HAL_Delay(DISPLAY_DELAY);
 }
 
 int main(void)
@@ -58,38 +101,24 @@ int main(void)
 	HAL_Init();
 	gpio_init();
 
-	const int button_delay = 25;
+	bool LED_is_on = false;
+	uint8_t count = 0;
 
+	/* Flags and logic live in main
+	 * */
 	while (1) {
-		// set PC0 to high
-		// GPIOC->ODR |= GPIO_ODR_OD0;
-		// GPIOC->ODR |= (1 << 0);
-
-		if (button_pressed()) {
-			HAL_Delay(button_delay);
-
-			if (button_pressed()) {
-				LED_on();
-			}
-
-			while(button_pressed()) {
-				HAL_Delay(button_delay);
-			}
+		if (button_is_pressed()) {
+			count_display(count);
+			LED_is_on = true;
+			count = (count + 1) % 8;
 		}
-
-		LED_off();
-
-		// // button is still not pressed
-		// while (!(GPIOA->IDR & GPIO_IDR_ID4))
-		// 	HAL_Delay(button_delay);
-		//
-		//
-		// HAL_Delay(button_delay);
-		//
-		// while (GPIOA->IDR & GPIO_IDR_ID4) {
-		// 	HAL_Delay(button_delay);
-		// }
-		//
-		//
-	} // end while
-} // end main
+		
+		/* Although this is not necessary (we can just repeatedly turn off LED when not needed), we
+		 * wanted to see if that is avoidable with boolean flags. This also makes the program more 
+		 * closely mimic an actual state machine. 
+		 * */
+		else if (LED_is_on) {
+			LED_turn_off();
+		}
+	}
+}
