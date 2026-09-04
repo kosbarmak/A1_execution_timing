bdcf05c33c8a92910b2faf3181efd03d1a4fd6ca
Alan Kusparmakov <alan.kusparmakov@gmail.com>
2026-09-03 17:04:40 -0700

    changed bit manipulations to macros

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,5 +1,9 @@
 #include "main.h"
+#include "stm32l476xx.h"
 #include "stm32l4xx_hal.h"
+#include "stm32l4xx_hal_gpio.h"
+
+#define GPIO_MODER_MODE_OUTPUT 0b01
 
 int main(void)
 {
@@ -10,21 +14,39 @@ int main(void)
 	// effectively turn on the heart
 	RCC->AHB2ENR |= (RCC_AHB2ENR_GPIOAEN | RCC_AHB2ENR_GPIOCEN);
 
-	GPIOC->MODER &= ~(0b11 << (0 * 2));
-	GPIOC->MODER |= (0b01 << (0 * 2));
+	// Set PC0-2 as LED power oudput
+	GPIOC->MODER &= ~(GPIO_MODER_MODE0_Msk);
+	GPIOC->MODER |= (0b0000000000000000000000000000000000000000000000000000000000000001 << GPIO_MODER_MODE0_Pos);
+
+	GPIOC->MODER &= ~(GPIO_MODER_MODE1_Msk);
+	GPIOC->MODER |= (0b01 << GPIO_MODER_MODE1_Pos);
+
+	GPIOC->MODER &= ~(GPIO_MODER_MODE2_Msk);
+	GPIOC->MODER |= (0b01 << GPIO_MODER_MODE2_Pos);
+
+	// set button as input
+	GPIOA->MODER &= ~(GPIO_MODER_MODE4_Msk);
+	GPIOA->MODER |= (GPIO_MODE_INPUT << GPIO_MODER_MODE4_Pos);
 
 	while (1) {
 		// set PC0 to high
 		// GPIOC->ODR |= GPIO_ODR_OD0;
-		GPIOC->ODR |= (1 << 0);
+		// GPIOC->ODR |= (1 << 0);
 
-		while(1);
+		if (GPIOA->IDR & GPIO_IDR_ID4) {
+			GPIOC->ODR |= GPIO_ODR_OD0;
+			GPIOC->ODR |= GPIO_ODR_OD1;
+			GPIOC->ODR |= GPIO_ODR_OD2;
 
-		break; 
+		}
 
-		HAL_Delay(1000);
-		// GPIOC->ODR &= ~GPIO_ODR_OD0;
-		GPIOC->ODR &= ~(1 << 0);
-		HAL_Delay(1000);
+		else {
+			GPIOC->ODR &= ~GPIO_ODR_OD0;
+			GPIOC->ODR &= ~GPIO_ODR_OD1;
+			GPIOC->ODR &= ~GPIO_ODR_OD2;
+		}
+		HAL_Delay(100);
+
+
 	} // end while
 } // end main
--- build/Debug/CMakeFiles/clean_additional.cmake
+++ build/Debug/CMakeFiles/clean_additional.cmake
@@ -3,6 +3,6 @@ cmake_minimum_required(VERSION 3.16)
 
 if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
   file(REMOVE_RECURSE
-  "LED_cubemx_cmake.map"
+  "A1_execution_timing.map"
   )
 endif()
